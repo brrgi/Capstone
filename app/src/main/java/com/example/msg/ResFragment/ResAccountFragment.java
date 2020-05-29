cf7396281acb5bc74d01fa4c8a9fcc469a884c5f
dntjr4772 <43207760+dntjr4772@users.noreply.github.com>
2020-05-29 16:43:24 +0900

    Resaccount completed

--- app/src/main/java/com/example/msg/ResFragment/ResAccountFragment.java
+++ app/src/main/java/com/example/msg/ResFragment/ResAccountFragment.java
@@ -1,26 +1,44 @@
-
 package com.example.msg.ResFragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-        import androidx.annotation.NonNull;
-        import androidx.annotation.Nullable;
-        import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-        import com.example.msg.R;
+
+import com.example.msg.R;
+import com.example.msg.Sale.ResSalesHistoryActivity;
 
 
 public class ResAccountFragment extends Fragment {
     private View view;
+    private LinearLayout ressaleshistory;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_resaccount,container,false);
+        ressaleshistory=view.findViewById(R.id.resaccount_linearLayout_saleshistory);
+
+        ressaleshistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ResSalesHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         return view;
     }
 }
---
+++ app/src/main/java/com/example/msg/Sale/ResSalesHistoryActivity.java
@@ -0,0 +1,87 @@
+package com.example.msg.Sale;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.msg.R;
+import com.example.msg.accountFragment.CompletedSaleFagment;
+import com.example.msg.accountFragment.CompletedSaleResFagment;
+import com.example.msg.accountFragment.OnSaleFagment;
+import com.example.msg.accountFragment.OnSaleResFagment;
+import com.example.msg.accountFragment.TradingFagment;
+import com.example.msg.accountFragment.TradingResFagment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResSalesHistoryActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private OnSaleResFagment onSaleResFagment;
+    private CompletedSaleResFagment completedSaleResFagment;
+    private TradingResFagment tradingResFagment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_history);
+
+        viewPager=findViewById(R.id.saleshistory_viewpager);
+        tabLayout=findViewById(R.id.saleshistory_tablayout);
+
+        onSaleResFagment=new OnSaleResFagment();
+        tradingResFagment=new TradingResFagment();
+        completedSaleResFagment=new CompletedSaleResFagment();
+
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),0);
+        viewPagerAdapter.addFragment(onSaleResFagment,"판매중");
+        viewPagerAdapter.addFragment(tradingResFagment,"거래중");
+        viewPagerAdapter.addFragment(completedSaleResFagment,"판매완료");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments=new ArrayList<>();
+        private List<String> fragmentTitle=new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment,String title){
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
+        }
+    }
+}
