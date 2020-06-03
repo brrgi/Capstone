4d07dc983e232ab00d10d0a8fb46ac1cfee8e842
brrgi <48500985+brrgi@users.noreply.github.com>
2020-06-03 14:53:24 +0900

    커밋

--- app/src/main/java/com/example/msg/ResFragment/ResAccountBookFragment.java
+++ app/src/main/java/com/example/msg/ResFragment/ResAccountBookFragment.java
@@ -1,10 +1,20 @@
 package com.example.msg.ResFragment;
 
 
+import android.graphics.Color;
+        import android.graphics.Typeface;
         import android.os.Bundle;
+        import android.text.Html;
+        import android.text.SpannableString;
+        import android.text.Spanned;
+        import android.text.style.ForegroundColorSpan;
+        import android.text.style.StyleSpan;
+        import android.text.style.UnderlineSpan;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
         import androidx.annotation.NonNull;
         import androidx.annotation.Nullable;
@@ -12,14 +22,118 @@ package com.example.msg.ResFragment;
 
         import com.example.msg.R;
 
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.StackedBarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.StackedBarModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
 
 public class ResAccountBookFragment extends Fragment {
     private View view;
+    private TextView day;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_resaccountbook,container,false);
+
+        day=(TextView)view.findViewById(R.id.resaccountbook_textView_date);
+        SpannableString content = new SpannableString(day.getText().toString());
+
+// 저는이미 TextView 에 String 을 넣었기 때문에 위와 같이 TextView.getText().toString() 했음
+
+        content.setSpan(new UnderlineSpan(), 0, content.length(),0);
+        content.setSpan(new StyleSpan(Typeface.ITALIC), 0, content.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        day.setText(content);
+
+        BarChart mBarChart = (BarChart) view.findViewById(R.id.barchart);
+
+        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+
+        mBarChart.startAnimation();
+
+        ///
+        StackedBarChart mStackedBarChart = (StackedBarChart) view.findViewById(R.id.stackedbarchart);
+
+        StackedBarModel s1 = new StackedBarModel("12.4");
+
+        s1.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s1.addBar(new BarModel(2.3f, 0xFF56B7F1));
+        s1.addBar(new BarModel(2.3f, 0xFFCDA67F));
+
+        StackedBarModel s2 = new StackedBarModel("13.4");
+        s2.addBar(new BarModel(1.1f, 0xFF63CBB0));
+        s2.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        s2.addBar(new BarModel(0.7f, 0xFFCDA67F));
+
+        StackedBarModel s3 = new StackedBarModel("14.4");
+
+        s3.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s3.addBar(new BarModel(2.f, 0xFF56B7F1));
+        s3.addBar(new BarModel(3.3f, 0xFFCDA67F));
+
+        StackedBarModel s4 = new StackedBarModel("15.4");
+        s4.addBar(new BarModel(1.f, 0xFF63CBB0));
+        s4.addBar(new BarModel(4.2f, 0xFF56B7F1));
+        s4.addBar(new BarModel(2.1f, 0xFFCDA67F));
+
+        mStackedBarChart.addBar(s1);
+        mStackedBarChart.addBar(s2);
+        mStackedBarChart.addBar(s3);
+        mStackedBarChart.addBar(s4);
+
+        mStackedBarChart.startAnimation();
+
+        ////
+        PieChart mPieChart = (PieChart) view.findViewById(R.id.piechart);
+
+        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+
+        mPieChart.startAnimation();
+        ////
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) view.findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("5/23", 2.4f));
+        series.addPoint(new ValueLinePoint("5/24", 3.4f));
+        series.addPoint(new ValueLinePoint("5/25", .4f));
+        series.addPoint(new ValueLinePoint("5/26", 1.2f));
+        series.addPoint(new ValueLinePoint("5/27", 2.6f));
+        series.addPoint(new ValueLinePoint("5/28", 1.0f));
+        series.addPoint(new ValueLinePoint("5/29", 3.5f));
+        series.addPoint(new ValueLinePoint("5/30", 2.4f));
+        series.addPoint(new ValueLinePoint("5/31", 2.4f));
+        series.addPoint(new ValueLinePoint("6/1", 3.4f));
+        series.addPoint(new ValueLinePoint("6/2", 3.0f));
+        series.addPoint(new ValueLinePoint("6/3", 2.5f));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
         return view;
+
+
+
+
     }
+
+
 }
