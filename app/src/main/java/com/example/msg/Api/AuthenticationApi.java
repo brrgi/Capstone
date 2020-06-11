7ca64fa1bf2110f8b53321935bac186c78239dfa
ChunYS <48270425+ChunYS@users.noreply.github.com>
2020-06-12 02:38:43 +0900

    minor

--- app/src/main/java/com/example/msg/Api/AuthenticationApi.java
+++ app/src/main/java/com/example/msg/Api/AuthenticationApi.java
@@ -11,15 +11,11 @@ public class AuthenticationApi {
         if (user != null) return true;
         else return false;
     }
-    /*
-     로그인이 된 상태면 true를 아니면 false를 돌려주는 가벼운 함수입니다.
-     */
+     //로그인이 된 상태면 true를 아니면 false를 돌려주는 가벼운 함수입니다.
 
     public static String getCurrentUid() {
-        String str = firebaseAuth.getCurrentUser().getUid();
-        return str;
+        return firebaseAuth.getCurrentUser().getUid();
     }
-    /*
-    로그인이 된 상태라면 현재 유저의 uid를 스트링 형태로 돌려주는 가벼운 함수입니다.
-     */
+    //로그인이 된 상태라면 현재 유저의 uid를 스트링 형태로 돌려주는 가벼운 함수입니다.
+
 }
