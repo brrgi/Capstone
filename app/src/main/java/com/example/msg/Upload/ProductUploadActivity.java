a817374379f83c6979ddde0e3dce0daa41e81f71
ChunYS <48270425+ChunYS@users.noreply.github.com>
2020-06-24 18:28:06 +0900

    Update ProductUploadActivity.java

--- app/src/main/java/com/example/msg/Upload/ProductUploadActivity.java
+++ app/src/main/java/com/example/msg/Upload/ProductUploadActivity.java
@@ -266,30 +266,30 @@ public class ProductUploadActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //두번 클릭 방지 threshold 5초
-                if (SystemClock.elapsedRealtime() - mLastClickTime < 5000){
-                    return;
+                    //두번 클릭 방지 threshold 5초
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < 5000) {
+                        return;
+                    }
+                    mLastClickTime = SystemClock.elapsedRealtime();
+
+                    //
+                    Toast.makeText(getApplicationContext(), "상품 등록 중 입니다.", Toast.LENGTH_SHORT).show();
+                    userProductModel.title = title.getText().toString();
+                    userProductModel.p_description = specification.getText().toString();
+                    userProductModel.categorySmall = smallSpinner.getSelectedItem().toString();
+                    userProductModel.categoryBig = bigSpinner.getSelectedItem().toString();
+                    userProductModel.quantity = quantity.getText().toString();
+                    userProductModel.expiration_date = expireDate.getText().toString();
+                    userProductModel.completed = -1;
+                    userProductModel.longitude = longitude;
+                    userProductModel.latitude = latitude;
+                    userProductModel.saleDateYear = -1;
+                    userProductModel.saleDateMonth = -1;
+                    userProductModel.saleDateDay = -1;
+                    userProductModel.quality = quality;
+                    postUserProduct(userProductModel);
+
                 }
-                mLastClickTime = SystemClock.elapsedRealtime();
-
-                //
-                Toast.makeText(getApplicationContext(), "상품 등록 중 입니다.", Toast.LENGTH_SHORT).show();
-                userProductModel.title = title.getText().toString();
-                userProductModel.p_description = specification.getText().toString();
-                userProductModel.categorySmall = smallSpinner.getSelectedItem().toString();
-                userProductModel.categoryBig = bigSpinner.getSelectedItem().toString();
-                userProductModel.quantity = quantity.getText().toString();
-                userProductModel.expiration_date = expireDate.getText().toString();
-                userProductModel.completed = -1;
-                userProductModel.longitude = longitude;
-                userProductModel.latitude = latitude;
-                userProductModel.saleDateYear=-1;
-                userProductModel.saleDateMonth=-1;
-                userProductModel.saleDateDay=-1;
-                userProductModel.quality=quality;
-                postUserProduct(userProductModel);
-
-            }
         });
 
 
