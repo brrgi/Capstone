45e03bccb3dcf1bd8e86e7e4611a58f9f2b07727
brrgi <48500985+brrgi@users.noreply.github.com>
2020-06-24 16:52:30 +0900

    fgfg

--- app/src/main/java/com/example/msg/Upload/ProductUploadActivity.java
+++ app/src/main/java/com/example/msg/Upload/ProductUploadActivity.java
@@ -255,8 +255,6 @@ public class ProductUploadActivity extends AppCompatActivity {
                             1,
                             gpsLocationListener);
                 }
-
-
             }
         });
 
@@ -301,7 +299,6 @@ public class ProductUploadActivity extends AppCompatActivity {
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 monthOfYear++;
                 expireDate.setText(year + "년" + monthOfYear + "월" + dayOfMonth + "일");
-
             }
         };
     }
